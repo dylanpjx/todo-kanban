2c8e8dc24eccd4ca3c6f8185373a20b2af8a0f4e
dylanpjx <dylanpjx@gmail.com>
2020-07-25 21:02:54 +0800

    Updates

---
+++ precache-manifest.f08e3ba4efb28f83880f655583808d7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ce64b2490a01b632fe3a9d25fc47b0",
+    "url": "/todo-kanban/index.html"
+  },
+  {
+    "revision": "9fcf5c6a23043723bf46",
+    "url": "/todo-kanban/static/css/main.b06c9a26.chunk.css"
+  },
+  {
+    "revision": "32564cce55fc8eb6dee6",
+    "url": "/todo-kanban/static/js/2.54509c3d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-kanban/static/js/2.54509c3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fcf5c6a23043723bf46",
+    "url": "/todo-kanban/static/js/main.3b695be6.chunk.js"
+  },
+  {
+    "revision": "7047c5c8d6ef58d0e5a9",
+    "url": "/todo-kanban/static/js/runtime-main.6c970b09.js"
+  }
+]);
